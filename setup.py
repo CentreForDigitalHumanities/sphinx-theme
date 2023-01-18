dd0a18f2c987e4947d54ed3ef9dde4c0d548e26f
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2023-01-18 15:44:52 +0100

    moved actual code to subdir

--- setup.py
+++ setup.py
@@ -3,11 +3,22 @@ from setuptools import setup
 
 
 setup(
-    name='uu-sphinx-theme',
+    name='uu_sphinx_theme',
+    py_modules=['uu_sphinx_theme'],
+    packages=['uu_sphinx_theme'],
+    include_package_data=True,
+    package_data={'uu_sphinx_theme': [
+        'theme.conf',
+        '*.html',
+        'static/*.*',
+    ]},
     version='1.0.0',
     entry_points={
         "sphinx.html_themes": [
             "uu_sphinx_theme = uu_sphinx_theme"
         ]
-    }
+    },
+    install_requires=[
+        'sphinx'
+    ]
 )

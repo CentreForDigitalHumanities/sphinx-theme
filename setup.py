5faae31dfe555c0a753830827d7b8eb185775992
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2023-01-18 15:29:04 +0100

    changed entry points to docs syntax

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     name='uu-sphinx-theme',
     version='1.0.0',
     entry_points={
-        "sphinx.html_themes": {
-            "uu_sphinx_theme": "uu_sphinx_theme"
-        }
+        "sphinx.html_themes": [
+            "uu_sphinx_theme = uu_sphinx_theme"
+        ]
     }
 )
